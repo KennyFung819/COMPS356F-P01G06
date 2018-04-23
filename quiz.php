ea8ebe5df4bc9d4113b56b1c11f7a9c857d0bd5a
DESKTOP-H8C6MG8\chufu <chufung97819>
2018-04-23 14:11:57 +0800

    Change quiz from html to php for navbar reason

--- quiz.php
+++ quiz.php
@@ -1,153 +1,12 @@
-<?php
-
-function render($qs, $params) {
-	?>
-	<form class="quiz-form">
-		<?php foreach ($qs as $qi => $q) { ?>
-			<div class="quiz-question">
-				<div class="quiz-question-text">
-					<?php echo $qi + 1; ?>. 
-					<?php echo $q[0]; ?>
-				</div>
-				<div class="quiz-options">
-					<?php foreach ($q[1] as $oi => $opt) { ?>
-						<div class="quiz-option">
-							<?php
-								$checked = isset($params["submit"]) &&
-								           isset($params[$qi]) &&
-										   $params[$qi] == $oi;
-							?>
-							<input
-								class="quiz-option-radio"
-								type="radio"
-								name="<?php echo $qi; ?>"
-								value="<?php echo $oi; ?>"
-								<?php if ($checked) echo "checked" ?>
-								/>
-							<span class="quiz-option-text"><?php echo $opt[0]; ?></span>
-						</div>
-					<?php } ?>
-				</div>
-				<?php if (isset($params["submit"]) && !isset($params[$qi])) { ?>
-					<div class="alert alert-danger quiz-error-text">You have not answered this question.</div>
-				<?php } ?>
-			</div>
-		<?php } ?>
-		<input type="hidden" name="submit" value="1"/>
-		<input type="submit" value="submit" class="btn btn-primary btn-outline"/>
-	</form>
-	<?php
-}
-
-function calculate_score($questions, $params) {
-	if (!isset($params["submit"])) return ["error", 0];
-	
-	$sum = 0;
-	foreach ($questions as $qi => $q) {
-		if (!isset($params[$qi])) return ["error", 0];
-		$score = $q[1][$params[$qi]][1];
-		$sum = $sum + $score;
-	}
-	return ["ok", $sum];
-}
-
-function calculate_grade($score, $gradings, $default_str) {
-	foreach ($gradings as $_ => $grade) {
-		list($min_value, $max_value, $grade_str) = $grade;
-		if ($min_value <= $score && $score <= $max_value) {
-			return $grade_str;
-		}
-	}
-	return $default_str;
-}
-
-$questions = [
-    ["content of q1", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-    ["content of q2", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-    ["content of q3", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-	["content of q4", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-	["content of q5", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-	["content of q6", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-	["content of q7", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-	["content of q8", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-	["content of q9", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]],
-	["content of q10", [
-        ["option1 ", 0],
-        ["option2 ", 5],
-        ["option3 ", 10],
-		["option4 ", 20],
-    ]]
-
-];
-
-$gradings = [
-	[0, 49, "You are not cellfish."],
-	[50, 100, "You are quite cellfish sometimes."],
-	[101, 150, "You are quite annoying when you are being cellfish."],
-	[151, 200, "Come on! You shouldn't go outside again!"]
-];
-
-
-
-
-?>
-
-
-<!doctype html>
-<html>
-	<head>
+<!DOCTYPE html>
+<!-- saved from url=(0035)http://192.168.99.100:8000/quiz.php -->
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
 		<title>The cellfish quiz</title>
-		
-		<link rel="stylesheet" href="css/bootstrap.css">
-		<link rel="stylesheet" href="css/style.css">
-		<link rel="stylesheet" href="css/index.css">
+		<?php require_once "asset/header.html"; ?>
 		<style>
-		
+		html{
+			position:relative;
+		}
 h1 {
 	font-family: "Playfair Display", serif;
 	font-weight: 700;
@@ -187,46 +46,265 @@ h1 {
 	margin-bottom: 90px;
 
 }
+
 		</style>
 	</head>
-	<body
-		<div class="fh5co-main-nav fh5co-shadow" style="position: fixed; top: 0px; width: 100%; z-index: 99999;">
+	<body <div="" class="fh5co-main-nav fh5co-shadow" style="position: fixed; top: 0px; width: 100%; z-index: 99999;">
 				<div class="container-fluid">
 					<div class="fh5co-menu-1">
-						<a href="index.php" data-nav-section="home">Home</a>
-						<a href="about.php" data-nav-section="about">About</a>
+						<a href="http://192.168.99.100:8000/index.php" data-nav-section="home">Home</a>
+						<a href="http://192.168.99.100:8000/about.php" data-nav-section="about">About</a>
 					</div>
 					<div class="fh5co-logo">
-						<a href="index.php">Cellfish</a>
+						<a href="http://192.168.99.100:8000/index.php">Cellfish</a>
 					</div>
 					<div class="fh5co-menu-2">
-						<a href="news.php" data-nav-section="features">News</a>
-						<a href="games.php" data-nav-section="menu" class="active">Game</a>
+						<a href="http://192.168.99.100:8000/news.php" data-nav-section="features">News</a>
+						<a href="http://192.168.99.100:8000/games.php" data-nav-section="menu" class="active">Game</a>
 					</div>
 				</div>
-				
-		</div>
+
+
 		<main class="quiz-container">
-		
-		<?
-		list($status, $score) = calculate_score($questions, $_GET);
-		if ($status === "ok") {
-			?>
-			<h1>Cellfish Quiz Result</h1>
-			<p><?php echo calculate_grade($score, $gradings, "ERROR NOT FOUND"); ?></p>
-			<form class="quiz-form">
-				<input type="submit" value="back" class="btn btn-primary btn-outline">
-			</form>
-			<?php
-		} else {
-			?>
-			<h1>Cellfish Quiz</h1>
-			<p>blah blah blah blah blah blah blah blah blah blah
-			blah blah blah blah blah blah blah blah blah blah blah</p>
-			<?php
-			render($questions, $_GET);
-		}
-		?>
-		</main>
-	</body>
+
+					<h1>Cellfish Quiz</h1>
+			<p>a cellfish uses a mobile phone in a way that disregards the wishes of other people.</p>
+				<form class="quiz-form">
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					1.
+					How often do you take photo(s) of food even not all friends of you in this gathering doing so?			</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="0" value="0">
+							<span class="quiz-option-text">Never </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="0" value="1">
+							<span class="quiz-option-text">Sometimes </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="0" value="2">
+							<span class="quiz-option-text">Often </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="0" value="3">
+							<span class="quiz-option-text">Always </span>
+						</div>
+									</div>
+							</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					2.
+						How many photo(s) of food do you take when the food is being served?			</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="1" value="0">
+							<span class="quiz-option-text">Zero </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="1" value="1">
+							<span class="quiz-option-text">1 - 5 </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="1" value="2">
+							<span class="quiz-option-text">6- 10 </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="1" value="3">
+							<span class="quiz-option-text">11 - more </span>
+						</div>
+									</div>
+							</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					3.
+					How often do you play voice message or games on your phone loudly without using an earphone	in the public area?			</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="2" value="0">
+							<span class="quiz-option-text">Never </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="2" value="1">
+							<span class="quiz-option-text">Sometimes </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="2" value="2">
+							<span class="quiz-option-text">often </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="2" value="3">
+							<span class="quiz-option-text">Always </span>
+						</div>
+									</div>
+							</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					4.
+					What purpose would you use your phone while watching movie in the cinema?				</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="3" value="0">
+							<span class="quiz-option-text">I don't use it in this situation </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="3" value="1">
+							<span class="quiz-option-text">Messaging app (whatsapp, telegram, etc) </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="3" value="2">
+							<span class="quiz-option-text">Call or answer the call </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="3" value="3">
+							<span class="quiz-option-text">Messaging app and calling </span>
+						</div>
+									</div>
+							</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					5.
+						How often do you use your phone continuously in a gathering or dinning with people?			</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="4" value="0">
+							<span class="quiz-option-text">Never </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="4" value="1">
+							<span class="quiz-option-text">sometimes </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="4" value="2">
+							<span class="quiz-option-text">Often </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="4" value="3">
+							<span class="quiz-option-text">Always </span>
+						</div>
+									</div>
+							</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					6.
+					How often do you texting people or play games	when you are walking in the street?			</div>
+				<div class="quiz-options">
+					<div class="quiz-option">
+								<input class="quiz-option-radio" type="radio" name="4" value="0">
+	<span class="quiz-option-text">Never </span>
+</div>
+					<div class="quiz-option">
+								<input class="quiz-option-radio" type="radio" name="4" value="1">
+	<span class="quiz-option-text">sometimes </span>
+</div>
+					<div class="quiz-option">
+								<input class="quiz-option-radio" type="radio" name="4" value="2">
+	<span class="quiz-option-text">Often </span>
+</div>
+					<div class="quiz-option">
+								<input class="quiz-option-radio" type="radio" name="4" value="3">
+	<span class="quiz-option-text">Always </span>
+</div>
+			</div>
+	</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					7.
+					How long do you use your phone daily?				</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="6" value="0">
+							<span class="quiz-option-text">0 - 1 hour </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="6" value="1">
+							<span class="quiz-option-text">2 - 3 hours </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="6" value="2">
+							<span class="quiz-option-text">4 - 5 hours </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="6" value="3">
+							<span class="quiz-option-text">more than 5 hours </span>
+						</div>
+									</div>
+							</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					8.
+					How often do your family wait for you to start dining when you are using your phone? 			</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="0">
+					<span class="quiz-option-text">Never </span>
+				</div>
+									<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="1">
+					<span class="quiz-option-text">sometimes </span>
+				</div>
+									<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="2">
+					<span class="quiz-option-text">Often </span>
+				</div>
+									<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="3">
+					<span class="quiz-option-text">Always </span>
+				</div>
+							</div>
+					</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					9.
+					How often do you use your phone when you are driving?				</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="0">
+					<span class="quiz-option-text">Never </span>
+				</div>
+									<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="2">
+					<span class="quiz-option-text">sometimes </span>
+				</div>
+									<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="4">
+					<span class="quiz-option-text">Often </span>
+				</div>
+									<div class="quiz-option">
+												<input class="quiz-option-radio" type="radio" name="4" value="6">
+					<span class="quiz-option-text">Always </span>
+				</div>
+							</div>
+					</div>
+					<div class="quiz-question">
+				<div class="quiz-question-text">
+					10.
+					Do you think the way that you use your phone affect the relationship with friend/family?				</div>
+				<div class="quiz-options">
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="9" value="0">
+							<span class="quiz-option-text">No </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="9" value="1">
+							<span class="quiz-option-text">Slightly </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="9" value="2">
+							<span class="quiz-option-text">Moderate </span>
+						</div>
+											<div class="quiz-option">
+														<input class="quiz-option-radio" type="radio" name="9" value="3">
+							<span class="quiz-option-text">Seriously </span>
+						</div>
+									</div>
+							</div>
+				<input type="hidden" name="submit" value="1">
+		<input type="submit" value="submit" class="btn btn-primary btn-outline">
+	</form>
+			</main>
+<?php require_once "asset/footer.html"?>
+</body>
+
 </html>
