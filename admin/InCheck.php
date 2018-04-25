d633babc182ba5b9868505f2eef63424bc83f81f
DESKTOP-H8C6MG8\chufu <chufung97819>
2018-04-25 15:13:42 +0800

    Reverse InCheck.php to previous version #admin

--- admin/InCheck.php
+++ admin/InCheck.php
@@ -1,59 +1,33 @@
 <?php
-<<<<<<< HEAD
-require_once('../database.php');
-
-=======
 	require_once('../database.php');
-
 	$aaa = "Insert into Post (Id,Title,Content,Introduction,type,likeNo,DateTime,Image) Values(" ;
->>>>>>> Search(Integrate)
 $_POST['likeNo']=0;
 $_POST['DateTime']=date("Y-m-d G:i:s");
 
-var_dump($_POST);
-
-/*
-'Id' => string '123123123' (length=9)
-  'Title' => string 'aaa' (length=3)
-  'Content' => string 'bbb' (length=3)
-  'Introduction' => string 'cc' (length=2)
-  'type' => string 'ddd' (length=3)
-  'Tag' => string 'aaaa' (length=4)
-  'likeNo' => int 0
-  'DateTime' => string '2018-02-28 8:59:58' (length=18)
-*/
-
-
-$stmt = "Insert into Post (Id,Title,Content,Introduction,type,likeNo,DateTime,Image) Values (:Id, :Title, :Content, :Introduction, :type, :Tag, :likeNo, :DateTime)";
-
-
-
-foreach($_POST as $key => $value){
-	if(strcmp('Tag',$key)){
-		$acc .= "'" . $value . "',";
+	foreach($_POST as $key => $value){
+		if(strcmp('Tag',$key)){
+	    $aaa .= "'".$value."',";
+		}
 	}
-}
 
+	if($_FILES['Image']['tmp_name']){
+	$aaa .= "'" .addslashes(file_get_contents($_FILES['Image']['tmp_name']))."');";
+	}else{
+		$aaa .="'');";
+	}
+	$sth = $conn->prepare($aaa);
+	echo $aaa;
+    print_r($sth->execute());
 
-
-if($_FILES['Image']['tmp_name']) {
-	$acc .= "'" . addslashes(file_get_contents($_FILES['Image']['tmp_name'])) . "');";
-} else {
-	$acc .="'');";
-}
-
-$sth = $conn->prepare($acc);
-//echo $aaa;
-print_r($sth->execute());
-
-//Inserting tags to database
+		//Inserting tags to database
 $tag=explode(' ',$_POST['Tag']);
 foreach($tag as $element){
-	$sql = "Insert into Tag values ('".$_POST['Id']."','".$element."');";
-	$sql = $conn->prepare($sql);
-	print_r($sql->execute());
+		$sql = "Insert into Tag values ('".$_POST['Id']."','".$element."');";
+		$sql = $conn->prepare($sql);
+		print_r($sql->execute());
 }
 
 
-// header('Location: addpost.php');
+	header('Location: addpost.php');
+
 ?>
